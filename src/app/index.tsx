8ed63953594e0ef6d3fe525f66e59c7b3d346b2f
Diego Fávero <die.favero@outlook.com>
2025-07-18 17:31:54 -0300

    feat: add header component and global styles with Tailwind CSS
    - Implemented a new Header component with navigation and location display.
    - Added global CSS styles using Tailwind CSS.
    - Configured Tailwind CSS in the project with a new configuration file.
    - Included TypeScript definitions for Nativewind.

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { ScrollView, View } from "react-native";
+import { Header } from "../components/header";
+
+import Constants from 'expo-constants';
+
+const statusBarHeight = Constants.statusBarHeight
+ 
+export default function Index() {
+  return (
+    <ScrollView style={{ flex: 1 }} className="bg-slate-200" showsVerticalScrollIndicator={false}>
+      <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8}}>
+        <Header/>
+      </View>
+    </ScrollView>
+  );
+}
